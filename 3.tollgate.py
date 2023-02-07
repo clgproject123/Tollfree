b936d3d94911cb6a1a74bad7281a973fdb92f8a9
clgproject123 <114796422+clgproject123@users.noreply.github.com>
2023-02-07 19:28:14 +0530

    Update 3.tollgate.py

--- 3.tollgate.py
+++ 3.tollgate.py
@@ -35,15 +35,9 @@ def tollGate():
     print('Choose the vehicle type')
     vehicleType = int(input(' 1.Bike \n 2.car \n 3.lorry \n 4.other \n:'))
 
-    if(vehicleType == 1):
-         VehicleType(motorTypes[1])
-    elif(vehicleType == 2):
-         VehicleType(motorTypes[2])
-    elif(vehicleType == 3):
-         VehicleType(motorTypes[3])
-    elif(vehicleType == 4):
-         VehicleType(motorTypes[4])
+    if(vehicleType <= 4):
+        VehicleType(motorTypes.get(vehicleType))
     else:
-         print('Please choose the correct option')
+        print('Please choose the correct option')
 
 tollGate()
